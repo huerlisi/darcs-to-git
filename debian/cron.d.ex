d2fe7a08a9427c34e5d3922edb666ec063bbb7f6
Simon Hürlimann (Cyt) <simon.huerlimann@cyt.ch>
2009-01-02 18:40:50 +0100

    add debian directory as generated by dh_make

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the darcs-to-git package
+#
+0 4	* * *	root	darcs-to-git_maintenance
---
+++ debian/darcs-to-git-default.ex
@@ -0,0 +1,10 @@
+# Defaults for darcs-to-git initscript
+# sourced by /etc/init.d/darcs-to-git
+# installed at /etc/default/darcs-to-git by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
